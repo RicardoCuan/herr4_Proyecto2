6b0952716c067d3547e488e1de2fa1cab98bd328
Edgar <edgari1697@gmail.com>
2020-12-17 02:12:42 -0500

    Se le agrego el sensor de huella dactilar

--- app/src/main/java/com/ricardocuan/proyecto2_sensores/MainActivity.java
+++ app/src/main/java/com/ricardocuan/proyecto2_sensores/MainActivity.java
@@ -1,13 +1,80 @@
 package com.ricardocuan.proyecto2_sensores;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+
+    //UI Views
+    private TextView authStatusTv;
+    private Button authBtn;
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init UI views
+        authStatusTv = findViewById(R.id.authStatusTv);
+        authBtn = findViewById(R.id.authBtn);
+
+        //init bio metric
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                //error authenticating, stop tasks that requires auth
+                authStatusTv.setText("Error de autenticacion: " + errString);
+                Toast.makeText(MainActivity.this, "Error de autenticacion: " + errString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                //authentication succeed, continue tasts that requires auth
+                authStatusTv.setText("Autenticacion exitosa!");
+                Toast.makeText(MainActivity.this, "Autenticacion exitosa", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //failed authenticating, stop tasks that requires auth
+                authStatusTv.setText("Autenticacion Fallida");
+                Toast.makeText(MainActivity.this, "Autenticacion Fallida!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //setup title,description on auth dialog
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Autenticacion biometrica")
+                .setSubtitle("Login utilizando autenticacion por huella")
+                .setNegativeButtonText("Contraseña de usuario")
+                .build();
+
+        //handle authBtn click, start authentication
+        authBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //show auth dialog
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
     }
 }
