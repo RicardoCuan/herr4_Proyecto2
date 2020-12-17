b7757e9ec715cf67f1ea2b8a3ad11eb0cb76c236
JAPL1499 <65917258+JAPL1499@users.noreply.github.com>
2020-12-17 13:36:08 -0500

    Termometro terminado

--- app/src/main/java/com/ricardocuan/proyecto2_sensores/MainActivity.java
+++ app/src/main/java/com/ricardocuan/proyecto2_sensores/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +18,7 @@ import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //UI Views
     private TextView authStatusTv;
@@ -22,11 +27,28 @@ public class MainActivity extends AppCompatActivity {
     private Executor executor;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
+    //Sensor temperature
+    private TextView textView;
+    private SensorManager sensorManager;
+    private Sensor tempSensor;
+    private boolean isTempSensorAvailible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Sensor Temperature
+        textView = findViewById(R.id.textView2);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)!=null){
+            tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            isTempSensorAvailible = true;
+        }else {
+            textView.setText("Temperature Sensor is not Availible");
+            isTempSensorAvailible = false;
+        }
 
         //init UI views
         authStatusTv = findViewById(R.id.authStatusTv);
@@ -76,5 +98,30 @@ public class MainActivity extends AppCompatActivity {
                 biometricPrompt.authenticate(promptInfo);
             }
         });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        textView.setText(sensorEvent.values[0]+" °C");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isTempSensorAvailible){
+            sensorManager.registerListener(this, tempSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isTempSensorAvailible){
+            sensorManager.unregisterListener(this);
+        }
     }
 }
