b2f9cd8c386b43da814b20116f51824fb314cccb
Ricardo Cuan <ricardo.cuan@aiesec.net>
2020-12-17 22:25:08 -0500

    📦 NEM: Geolocalizacion

--- app/src/main/java/com/ricardocuan/proyecto2_sensores/MainActivity.java
+++ app/src/main/java/com/ricardocuan/proyecto2_sensores/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener,LocationListener  {
 
     //UI Views
     private TextView authStatusTv;
@@ -33,10 +33,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor tempSensor;
     private boolean isTempSensorAvailible;
 
+    // Geolocalización
+    static final long TIEMPO_MIN = 10 * 1000 ; // 10 segundos
+    static final long DISTANCIA_MIN = 5; // 5 metros
+    static final String[] A = { "n/d", "preciso", "impreciso" };
+    static final String[] P = { "n/d", "bajo", "medio","alto" };
+    static final String[] E = { "fuera de servicio",
+            "temporalmente no disponible ", "disponible" };
+    LocationManager manejador;
+    String proveedor;
+    TextView salida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Geolocalización - START
+
+        salida = findViewById(R.id.salida);
+        manejador = (LocationManager) getSystemService(LOCATION_SERVICE);
+        log("Proveedores de localización: \n ");
+        muestraProveedores();
+
+        Criteria criterio = new Criteria();
+        criterio.setCostAllowed(false);
+        criterio.setAltitudeRequired(false);
+        criterio.setAccuracy(Criteria.ACCURACY_FINE);
+        proveedor = manejador.getBestProvider(criterio, true);
+        log("Mejor proveedor: " + proveedor + "\n");
+        log("Comenzamos con la última localización conocida:");
+        Location localizacion = manejador.getLastKnownLocation(proveedor);
+        muestraLocaliz(localizacion);
+        // Geolocalización - END
+
 
         //Sensor Temperature
         textView = findViewById(R.id.textView2);
@@ -115,6 +145,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (isTempSensorAvailible){
             sensorManager.registerListener(this, tempSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
+
+        // Geolocalización
+        manejador.requestLocationUpdates(proveedor, TIEMPO_MIN, DISTANCIA_MIN, this);
     }
 
     @Override
@@ -123,5 +156,66 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (isTempSensorAvailible){
             sensorManager.unregisterListener(this);
         }
+
+        // Geolocalización
+        manejador.removeUpdates(this);
     }
+
+    // Geolocalización - START
+    // Métodos de la interfaz LocationListener
+    @Override public void onLocationChanged(Location location) {
+        log("Nueva localización: ");
+        muestraLocaliz(location);
+    }
+
+    @Override public void onProviderDisabled(String proveedor) {
+        log("Proveedor deshabilitado: " + proveedor + "\n");
+    }
+
+    @Override public void onProviderEnabled(String proveedor) {
+        log("Proveedor habilitado: " + proveedor + "\n");
+    }
+
+    @Override public void onStatusChanged(String proveedor, int estado,
+                                          Bundle extras) {
+        log("Cambia estado proveedor: " + proveedor + ", estado="
+                + E[Math.max(0, estado)] + ", extras=" + extras + "\n");
+    }
+
+    // Métodos para mostrar información
+    private void log(String cadena) {
+        salida.append(cadena + "\n");
+    }
+
+    private void muestraLocaliz(Location localizacion) {
+        if (localizacion == null)
+            log("Localización desconocida\n");
+        else
+            log(localizacion.toString() + "\n");
+    }
+
+    private void muestraProveedores() {
+        log("Proveedores de localización: \n ");
+        List<String> proveedores = manejador.getAllProviders();
+        for (String proveedor : proveedores) {
+            muestraProveedor(proveedor);
+        }
+    }
+
+    private void muestraProveedor(String proveedor) {
+        LocationProvider info = manejador.getProvider(proveedor);
+        log("LocationProvider[ " + "getName=" + info.getName()
+                + ", isProviderEnabled="
+                + manejador.isProviderEnabled(proveedor) + ", getAccuracy="
+                + A[Math.max(0, info.getAccuracy())] + ", getPowerRequirement="
+                + P[Math.max(0, info.getPowerRequirement())]
+                + ", hasMonetaryCost=" + info.hasMonetaryCost()
+                + ", requiresCell=" + info.requiresCell()
+                + ", requiresNetwork=" + info.requiresNetwork()
+                + ", requiresSatellite=" + info.requiresSatellite()
+                + ", supportsAltitude=" + info.supportsAltitude()
+                + ", supportsBearing=" + info.supportsBearing()
+                + ", supportsSpeed=" + info.supportsSpeed() + " ]\n");
+    }
+    // Geolocalización - EMD
 }
